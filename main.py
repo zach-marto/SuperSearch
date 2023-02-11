d1d4a63e89e21ec940ef5dfc32c75d09a03154ec
Zach Marto <zachm7115@gmail.com>
2023-02-11 15:03:55 -0500

    Initial commit: The main and search_and_export files.

---
+++ main.py
@@ -0,0 +1,6 @@
+import search_and_export
+
+if __name__ == '__main__':
+    search_and_export.printEx()
+    import multiprocessing
+    print(multiprocessing.cpu_count())
---
+++ search_and_export.py
@@ -0,0 +1,12 @@
+class exportSettings:
+    def __init__(self):
+        self.extension = ""
+        self.search_dir = ""
+        self.export_dir = ""
+        self.extensions_dir = ""
+        self.index_recursively = True
+
+
+
+def printEx():
+    print("test print")
